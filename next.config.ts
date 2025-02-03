a4e3999a7375bfa4e5f0db47c729f5fe640f61b0
Nicholas Rice <nicholasroyrice@gmail.com>
2025-02-02 21:55:07 -0800

    add base path to config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/nummyrice.github.io",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
